066fad264d0d386d9d2c643e6a5e322157387c50
Ayodeji Quadri <dejiovu@gmail.com>
2019-02-12 10:49:05 +0100

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require('mongodb');
@@ -58,6 +59,31 @@ app.delete("/todos/:id",(req,res)=>{
         }
         res.send({todo});
     }).catch((e)=> res.status(400).send());
+});
+
+app.patch("/todos/:id",(req,res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body,['text','completed']);
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send('Invalid Id');
+    }
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    //return the updated todo
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{
+        if(!todo){
+            res.status(404).send()
+        }
+        res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
 });
 
 app.listen(port,()=>{
