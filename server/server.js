7f635b7e86215ada61a6d3b71c4a79acd5373de5
Ayodeji Quadri <dejiovu@gmail.com>
2019-02-13 22:10:46 +0100

    Add GET /users/me route and authnetication middleware

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -16,7 +17,6 @@ app.use(bodyParser.json());
 app.post('/todos',(req,res)=>{
     var todo = new Todo({text: req.body.text});
     
-
     todo.save().then((doc)=>{
         res.send(doc);
     },(e)=>{
@@ -97,6 +97,12 @@ app.post("/users",(req,res)=>{
         res.header('x-auth',token).send(user);
     })
     .catch((e)=>res.status(400).send(e));
+});
+
+
+
+app.get("/users/me",authenticate,(req,res)=>{
+    res.send(req.user);
 });
 
 app.listen(port,()=>{
