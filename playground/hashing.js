74db47a97d6f8533c515973d87a1fdfa65660ff0
Ayodeji Quadri <dejiovu@gmail.com>
2019-02-14 13:35:10 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,26 @@
 const {SHA256} = require('crypto-js');
 const jwt =  require('jsonwebtoken'); 
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
-var token = jwt.sign(data, '123abc');
-console.log(token);
+// var password = '123abc';
+// bcrypt.genSalt(10,(err,salt)=>{
+//     bcrypt.hash(password,salt,(err,hash)=>{
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token,'123abcc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$AUZgbjwgCS7b4CcELHqz1.U5BswQTOnilkjFdRUBnjjBvzUMOn6CC';
+bcrypt.compare(password,hashedPassword, (err,res)=>{
+    console.log(res);
+});
+// var data = {
+//     id:10
+// };
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123abcc');
+// console.log('decoded', decoded);
 
 
 
